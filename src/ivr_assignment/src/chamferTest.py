29ed588de41ee1f98855be14d913881a1ea90332
JDfaker <a920423588@gmail.com>
2019-11-11 11:32:13 +0000

    templateMatch test

---
+++ src/ivr_assignment/src/chamferTest.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import roslib
+import sys
+import rospy
+import cv2
+import numpy as np
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from std_msgs.msg import Float64MultiArray, Float64
+from cv_bridge import CvBridge, CvBridgeError
+
+
+class image_converter:
+
+  # Defines publisher and subscriber
+  def __init__(self):
+    # initialize the node named image_processing
+    rospy.init_node('image_processing', anonymous=True)
+    self.image_pub1 = rospy.Publisher("image_topic1",Image, queue_size = 1)
+    self.image_sub1 = rospy.Subscriber("/camera1/robot/image_raw",Image,self.callback1)
+    # initialize the bridge between openCV and ROS
+    self.bridge = CvBridge()
+
+
+  # Recieve data from camera 1, process it, and publish
+  def callback1(self,data):
+    # Recieve the image
+    try:
+      self.cv_image1 = self.bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError as e:
+      print(e)
+    
+    mask = self.detect_orange(self.cv_image2)
+    template = cv2.imread("image_crop.png",0)
+    self.find_target(mask,template)
+    # Publish the results
+    try: 
+      self.image_pub1.publish(self.bridge.cv2_to_imgmsg(self.cv_image1, "bgr8"))
+    except CvBridgeError as e:
+      print(e)
+
+  def detect_orange(self,image):
+    mask = cv2.inRange(image,(50,100,110),(90,185,220))
+    #cv2.imshow("w",mask)
+    #cv2.waitKey(1)
+    return mask
+    
+
+  def find_target(self,image,template):
+    res =  cv2.matchTemplate(image,template,1)
+# call the class
+def main(args):
+  ic = image_converter()
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print("Shutting down")
+  cv2.destroyAllWindows()
+
+# run the code if the node is called
+if __name__ == '__main__':
+    main(sys.argv)
+
+
