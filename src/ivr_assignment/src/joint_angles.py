bed1ef156e540ad42b2f38e5e59a83050c97c3cd
JDfaker <a920423588@gmail.com>
2019-11-20 13:38:45 +0000

    fixed joint_angle

--- src/ivr_assignment/src/joint_angles.py
+++ src/ivr_assignment/src/joint_angles.py
@@ -29,7 +29,7 @@ class image_converter:
     except CvBridgeError as e:
       print(e)
 
-    green_position = np.array([1,1,2.5])
+    green_position = np.array([0,0,5])
     res = least_squares(self.fun_Kinematic,green_position,self.jacobian, bounds = ([-np.inf,-np.inf,-np.inf],np.inf))
     print(res.x)
     try: 
@@ -40,13 +40,13 @@ class image_converter:
   def fun_Kinematic(self,q):
     return np.array(
       [3*math.sin(q[0])*math.sin(q[1])*math.cos(q[2]) + 3*math.cos(q[0])*math.sin(q[2]),
-      -3*math.sin(q[1])*math.cos(q[0])*math.cos(q[2]) + 3*math.sin(q[0])*math.cos(q[2]),
+      -3*math.cos(q[0])*math.sin(q[1])*math.cos(q[2]) + 3*math.sin(q[0])*math.sin(q[2]),
       3*math.cos(q[1])*math.cos(q[2]) + 2])
 
   def jacobian(self,q):
-    return np.array([[3*math.cos(q[0])*math.sin(q[1])*math.cos(q[2]) - 3*math.sin(q[1])*math.cos(q[2]), 3*math.sin(q[0])*math.cos(q[1])*math.cos(q[2]), -3*math.sin(q[0])*math.sin(q[1])*math.sin(q[2]) + 3*math.cos(q[0])*math.cos(q[1])],
-                    [3*math.sin(q[0])*math.sin(q[1])*math.cos(q[2]) + 3*math.cos(q[0])*math.cos(q[2]), -3*math.cos(q[0])*math.cos(q[1])*math.cos(q[2]), 3*math.sin(q[1])*math.cos(q[0])*math.cos(q[2]) - 3*math.sin(q[0])*math.sin(q[2])],
-                    [0, -3*math.sin(q[1])*math.cos(q[2]), -3*math.sin(q[2])*math.cos(q[1])]])
+    return np.array([[3*math.cos(q[0])*math.sin(q[1])*math.cos(q[2]) - 3*math.sin(q[1])*math.cos(q[2]), 3*math.sin(q[0])*math.cos(q[1])*math.cos(q[2]), -3*math.sin(q[0])*math.sin(q[1])*math.sin(q[2]) + 3*math.cos(q[0])*math.cos(q[2])],
+                    [3*math.sin(q[0])*math.sin(q[1])*math.cos(q[2]) + 3*math.cos(q[0])*math.cos(q[2]), -3*math.cos(q[0])*math.cos(q[1])*math.cos(q[2]), 3*math.cos(q[0])*math.sin(q[0])*math.sin(q[2]) + 3*math.sin(q[0])*math.cos(q[2])],
+                    [0, -3*math.sin(q[1])*math.cos(q[2]), -3*math.cos(q[1])*math.sin(q[2])]])
 
 # call the class
 def main(args):
